13d9cd1f952f824b2ee55a0257603cccf74ab9bd
rilian <dmitriyis@gmail.com>
2013-05-26 13:38:50 +0300

    initialize "E" db

--- parse.rb
+++ parse.rb
@@ -1,10 +1,34 @@
 require 'sqlite3'
 
+# Set up database schema and perform cleanup
 puts 'Initializing DB'
 
 @db = SQLite3::Database.new('postcodes.sqlite3')
 
-# Set up database schema and perform cleanup
-@db.execute 'DROP TABLE IF EXISTS postcodes;'
+# Codelist.xls
+@db.execute 'DROP TABLE IF EXISTS es;'
+@db.execute 'CREATE TABLE es (
+  e TEXT PRIMARY KEY,
+  name TEXT
+);'
 
-@db.execute 'CREATE TABLE postcodes (code TEXT PRIMARY KEY, lat double, lon double);'
+# Postcodes API
+@db.execute 'DROP TABLE IF EXISTS areas;'
+@db.execute 'CREATE TABLE areas (
+  code TEXT PRIMARY KEY,
+  lat double,
+  lon double,
+  town TEXT,
+  region TEXT,
+  country TEXT,
+  district_id INTEGER
+);'
+
+@db.execute 'DROP TABLE IF EXISTS postcodes;'
+@db.execute 'CREATE TABLE postcodes (
+  code TEXT PRIMARY KEY,
+  lat double,
+  lon double
+);'
+
+puts 'DB Initialized'
