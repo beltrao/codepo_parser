f9fd9c50f39716067adb635e64a388b957385cad
rilian <dmitriyis@gmail.com>
2013-06-10 18:50:23 +0300

    all codelist files now included; improved logging

--- prepare_sheets.rb
+++ prepare_sheets.rb
@@ -4,33 +4,41 @@ require 'debugger'
 load 'init_db.rb'
 
 EXCLUDED_CODELIST_SHEETS = %w[Metadata AREA_CODES]
-CODELIST_FILE = 'codepo_gb/Doc/Codelist.xls'
+DATA_DIR = 'codepo_gb/Doc'
+CODELIST_FILES = %w[Codelist.xls NHS_Codelist.xls]
 
-raise "#{CODELIST_FILE} not exists!" unless File.exists?(CODELIST_FILE)
+CODELIST_FILES.each do |file_name|
+  puts "Working on file #{file_name}"
 
-codelist = Roo::Excel.new(CODELIST_FILE)
+  file_path = File.join(DATA_DIR, file_name)
 
-codelist.sheets.each do |sheet_name|
-  next if EXCLUDED_CODELIST_SHEETS.include?(sheet_name)
+  raise "#{file_name} not exists!" unless File.exists?(file_path)
 
-  puts "Working on #{sheet_name}"
-  codelist.default_sheet = sheet_name
+  codelist = Roo::Excel.new(file_path)
 
-  row_idx = 1
-  while row_idx < codelist.last_row do
-    name = codelist.cell(row_idx, 1)
-    e_value = codelist.cell(row_idx, 2)
-    query = "INSERT INTO es (e, name) VALUES ('#{e_value.gsub("'", "\\'")}', '#{name.gsub("'", "\\'")}');"
-    begin
-      @db.execute query
-    rescue Exception => ex
-      puts 'Issues with query:'
-      puts query
-      puts ex.message
+  codelist.sheets.each do |sheet_name|
+    next if EXCLUDED_CODELIST_SHEETS.include?(sheet_name)
+
+    puts "Working on sheet #{sheet_name}"
+    codelist.default_sheet = sheet_name
+
+    row_idx = 1
+    while row_idx < codelist.last_row do
+      name = codelist.cell(row_idx, 1)
+      e_value = codelist.cell(row_idx, 2)
+      query = "INSERT INTO es (e, name) VALUES ('#{e_value.gsub("'", "\\'")}', '#{name.gsub("'", "\\'")}');"
+      begin
+        @db.execute query
+      rescue Exception => ex
+        puts "Issues with query #{query}:"
+        puts ex.message
+      end
+
+      row_idx += 1
     end
 
-    row_idx += 1
+    puts "Successfully parsed sheet #{sheet_name}"
   end
+  puts "Successfully parsed file #{file_name}"
 end
 
-puts "Successfully parsed #{CODELIST_FILE}"
