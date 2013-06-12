21e7733b2fda00218306498d1eaed9f04a75eba5
rilian <dmitriyis@gmail.com>
2013-06-12 18:19:05 +0300

    remove existence check

--- 3_produce_export.rb
+++ 3_produce_export.rb
@@ -7,7 +7,7 @@ export_sql = File.open('export.pgsql', 'w')
 postcodes = @db.execute 'select * from postcodes'
 
 postcodes.each do |code, lat, lon|
-  export_sql.write "INSERT INTO postcodes(code, lat,lon) VALUES ('#{code}', #{lat}, #{lon}) WHERE NOT EXISTS (SELECT * FROM postcodes WHERE code='#{code}');"
+  export_sql.write "INSERT INTO postcodes(code, lat,lon) VALUES ('#{code}', #{lat}, #{lon});"
 end
 
 export_sql.close
