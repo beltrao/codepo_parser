2ea2b9246fdb8f15c7576e1fd74c6e61384a5d77
rilian <dmitriyis@gmail.com>
2013-07-04 00:09:27 +0300

    readme; export to separate table name

--- 3_produce_export.rb
+++ 3_produce_export.rb
@@ -7,7 +7,7 @@ export_sql = File.open('export.pgsql', 'w')
 postcodes = @db.execute 'select * from postcodes'
 
 postcodes.each do |code, lat, lon|
-  export_sql.write "INSERT INTO postcodes(code, lat,lon) VALUES ('#{code}', #{lat}, #{lon});"
+  export_sql.write "INSERT INTO postcodes_codepo_gb_may_2013(code, lat,lon) VALUES ('#{code}', #{lat}, #{lon});"
 end
 
 export_sql.close
---
+++ README.md
@@ -0,0 +1,6 @@
+CodePO parser
+=============
+
+1) run scripts 1, 3, 3
+
+2) import created sql file into your postcodes database `psql databasename < export.pgsql`
